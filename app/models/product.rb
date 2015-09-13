e59e57e06175fedd0068bf7bd901ac9f12973b37
Oleg Bloshenkin <darelvc@gmail.com>
2015-09-13 21:18:09 +0300

    add validates to new form

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+	validates :title, :description, :image_url, :presence => { :message => 'Не может быть пустым'}
+	validates :price, numericality: {greater_than_or_equal_to: 0.01, message: 'не является числом'}
+	validates :title, uniqueness: {message: 'не уникальный заголовок'}
+	validates :image_url, allow_blank: true, format: {
+		with: %r{\.(jpg|png|gif)\Z}i,
+		message: 'URL должен указывать на изображение формата gif, png, jpg'
+	}
 end
